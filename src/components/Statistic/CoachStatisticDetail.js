1ef38ce3fcc18db1a206b95c34f51d015cef49d8
lqaxx7799 <lqaxx7799@gmail.com>
2020-12-14 08:22:12 +0700

    a

--- src/components/Statistic/CoachStatisticDetail.js
+++ src/components/Statistic/CoachStatisticDetail.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import _ from 'lodash';
 import {
+  Button,
   DataTable,
   Table,
   TableBody,
@@ -13,39 +14,48 @@ import {
 import { TRIP_DATA_TABLE_CONFIG } from '../../helpers/constants';
 
 const CoachStatisticDetail = (props) => {
+  const formattedTrips = _.map(_.get(props, 'data.listTrip'), trip => ({
+    ...trip,
+    coachModel: _.get(trip, 'coach.coachModel'),
+    route: `${_.get(trip, 'route.startingPoint')} - ${_.get(trip, 'route.destinationPoint')}`,
+    driverName: _.get(trip, 'employee1.employeeName'),
+    assistantName: _.get(trip, 'employee2.employeeName'),
+  }));
   return (
-    <div className='bx--row'>
-      <div className="bx--col-lg-16">
-        <DataTable
-          rows={_.get(props, 'data.listTrip')}
-          headers={TRIP_DATA_TABLE_CONFIG}
-          useZebraStyles={true}
-          render={({ rows, headers, getHeaderProps, getTableProps }) => (
-            <TableContainer title="DataTable">
-              <Table {...getTableProps()}>
-                <TableHead>
-                  <TableRow>
-                    {headers.map(header => (
-                      <TableHeader key={header.key}>
-                        {header.name}
-                      </TableHeader>
-                    ))}
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {rows.map(row => (
-                    <TableRow key={row.id}>
-                      {row.cells.map(cell => (
-                        <TableCell key={cell.id}>{cell.value}</TableCell>
-                      ))}
-                    </TableRow>
+    <DataTable
+      rows={formattedTrips}
+      headers={TRIP_DATA_TABLE_CONFIG}
+      useZebraStyles={true}
+      render={({ rows, headers, getHeaderProps, getTableProps }) => (
+        <TableContainer title="DataTable">
+          <Table {...getTableProps()}>
+            <TableHead>
+              <TableRow>
+                {headers.map(header => (
+                  <TableHeader key={header.key}>
+                    {header.name}
+                  </TableHeader>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map(row => (
+                <TableRow key={row.id}>
+                  {row.cells.map(cell => (
+                    cell.info.header !== 'action'
+                      ? <TableCell key={cell.id}>{cell.value}</TableCell>
+                      : (
+                        <TableCell key={cell.id}>
+                          <div></div>
+                        </TableCell>
+                      )
                   ))}
-                </TableBody>
-              </Table>
-            </TableContainer>)}
-        />
-      </div>
-    </div>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>)}
+    />
   );
 }
 
